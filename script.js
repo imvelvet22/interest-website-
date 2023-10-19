5fd9d640783a0a265b5c83ec0aec67ee3007a00e
imvelvet22 <clariezabautista@gmail.com>
2023-10-20 01:33:29 +0800

    update

--- script.js
+++ script.js
@@ -3,27 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.getElementById("menuButton");
     const closeButton = document.getElementById("closeButton");
     const navMenu = document.querySelector('nav ul');
-
+  
     function openmenu() {
-        sidemenu.style.right = "0";
-        closeButton.style.display = "block"; // Show the close button
-        menuButton.style.display = "none"; // Hide the menu button
+      sidemenu.style.right = "0";
+      closeButton.style.display = "block"; // Show the close button
+      menuButton.style.display = "none"; // Hide the menu button
+      navMenu.classList.add('show-menu');
     }
-
+  
     function closemenu() {
-        sidemenu.style.right = "-200px";
-        closeButton.style.display = "none"; // Hide the close button
-        menuButton.style.display = "block"; // Show the menu button
+      sidemenu.style.right = "-200px";
+      closeButton.style.display = "none"; // Hide the close button
+      menuButton.style.display = "block"; // Show the menu button
+      navMenu.classList.remove('show-menu');
     }
-
-    function openMenu() {
-        navMenu.classList.add('show-menu');
-    }
-
-    function closeMenu() {
-        navMenu.classList.remove('show-menu');
-    }
-
-    menuButton.addEventListener('click', openMenu);
-    closeButton.addEventListener('click', closeMenu);
-});
+  
+    menuButton.addEventListener('click', openmenu);
+    closeButton.addEventListener('click', closemenu);
+  });
+  
